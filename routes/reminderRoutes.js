a893dd405b1ff090bdfdd698cb7613c06fd6b23f
aarondima <aarondima@gmail.com>
2024-04-18 20:34:14 -0700

    fix more merge

--- routes/reminderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reminderController = require('../controller/reminder_controller'); // Adjust path if necessary
-
-// Define your routes here, for example:
-router.get('/', reminderController.list);
-
-module.exports = router;
